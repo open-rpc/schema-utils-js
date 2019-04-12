d5a9f301dae65ad41e99f7f9cf604f222804081c
Zachary Belford <belfordz66@gmail.com>
2019-04-11 22:59:15 -0700

    fix: add docs to methodCallValidator

--- src/method-call-validator/method-call-validator.ts
+++ src/method-call-validator/method-call-validator.ts
@@ -4,13 +4,30 @@ import { generateMethodParamId } from "../generate-method-id";
 import { types } from "@open-rpc/meta-schema";
 import ParameterValidationError from "./parameter-validation-error";
 
+/**
+ * A class to assist in validating method calls to an OpenRPC-based service. Generated Clients,
+ * Servers, and many others may want to expose the interface provided by an OpenRPC document.
+ * In doing so, use this class to easily create a re-useable validator for a particular method.
+ */
 export default class MethodCallValidator {
   private ajvValidator: Ajv.Ajv;
 
-  constructor(private schema: types.OpenRPC) {
+  /**
+   * @param document The OpenRPC document containing the methods whose calls we want validated.
+   *
+   * @example
+   * ```typescript
+   *
+   * import { petstore } from "@open-rpc/examples";
+   * const petStoreMethodCallValidator = new MethodCallValidator(petstore);
+   * // Go on and use it!
+   * ```
+   *
+   */
+  constructor(private document: types.OpenRPC) {
     this.ajvValidator = new Ajv();
 
-    schema.methods.forEach((method: types.MethodObject) => {
+    document.methods.forEach((method: types.MethodObject) => {
       const params = method.params as types.ContentDescriptorObject[];
       if (method.params === undefined) { return; }
 
@@ -22,8 +39,26 @@ export default class MethodCallValidator {
     });
   }
 
+  /**
+   * Validates a particular method call against the OpenRPC definition for the method.
+   *
+   * @param methodName the name of the method in the OpenRPC Document.
+   * @param params the param values that you want validated.
+   *
+   * @returns an array of parameter validation errors, or if there are none, an empty array.
+   *
+   * @example
+   * ```typescript
+   *
+   * import { petstore } from "@open-rpc/examples";
+   * const petStoreMethodCallValidator = new MethodCallValidator(petstore);
+   * const errors = petStoreMethodCallValidator.validate("list_pets", []);
+   * // errors.length === 0
+   * ```
+   *
+   */
   public validate(methodName: string, params: any[]): ParameterValidationError[] {
-    const method = _.find(this.schema.methods, { name: methodName }) as types.MethodObject;
+    const method = _.find(this.document.methods, { name: methodName }) as types.MethodObject;
 
     if (method.params === undefined) {
       return [];
