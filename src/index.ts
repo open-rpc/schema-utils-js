3ed93c993f77073203cf34c9cb988fd17a3f30fd
Zachary Belford <belfordz66@gmail.com>
2019-04-11 21:19:55 -0700

    fix(export): naming incorrect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import parseOpenRPCDocument from "./parse-open-rpc-document";
-import validateOpenRPCDocument from "./get-validation-errors";
+import validateOpenRPCDocument from "./validate-open-rpc-document";
 import { generateMethodParamId, generateMethodResultId } from "./generate-method-id";
 import { MethodCallValidator, ParameterValidationError } from "./method-call-validator";
 
