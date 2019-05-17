d0e0c8ba990b660fc0140b8288d5dbefd5a03d45
Zachary Belford <belfordz66@gmail.com>
2019-05-17 11:31:33 -0700

    chore: cleanup fs related

--- src/get-open-rpc-document.ts
+++ src/get-open-rpc-document.ts
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import { readJson } from "fs-extra";
+import { readJson, pathExists } from "fs-extra";
 import { OpenRPC } from "@open-rpc/meta-schema";
 
 type TGetOpenRPCDocument = (schema: string) => Promise<OpenRPC>;
@@ -13,14 +13,25 @@ const fetchUrlSchemaFile: TGetOpenRPCDocument = async (schema) => {
   }
 };
 
-const readSchemaFromFile: TGetOpenRPCDocument = async (schema) => {
+const readSchemaFromFile: TGetOpenRPCDocument = async (filePath: string) => {
+  let isCorrectPath: boolean;
   try {
-    return await readJson(schema) as OpenRPC;
+    isCorrectPath = await pathExists(filePath);
+  } catch (e) {
+    throw new Error(`problem reading the file: ${e.message}`);
+  }
+
+  if (isCorrectPath === false) {
+    throw new Error(`unable to find file ${filePath}`);
+  }
+
+  try {
+    return await readJson(filePath) as OpenRPC;
   } catch (e) {
     if (e.message.includes("SyntaxError")) {
-      throw new Error(`Failed to parse json in file ${schema}`);
+      throw new Error(`Failed to parse json in file ${filePath}`);
     } else {
-      throw new Error(`Unable to read openrpc.json file located at ${schema}`);
+      throw new Error(`Unable to read openrpc.json file located at ${filePath}`);
     }
   }
 };
