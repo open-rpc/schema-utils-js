8e38df16a8552575829c98735369967111ca12fc
Zachary Belford <belfordz66@gmail.com>
2020-07-07 16:16:50 -0700

    fix: add failing test

--- src/dereference-document.test.ts
+++ src/dereference-document.test.ts
@@ -1,6 +1,6 @@
 import * as _fs from "fs-extra";
 import makeDereferenceDocument from "./dereference-document";
-import { OpenrpcDocument, ContentDescriptorObject } from "@open-rpc/meta-schema";
+import { OpenrpcDocument, ContentDescriptorObject, JSONSchema } from "@open-rpc/meta-schema";
 
 const dereferenceDocument = makeDereferenceDocument();
 const fs: any = _fs;
@@ -92,5 +92,64 @@ describe("dereferenceDocument", () => {
     expect((document.methods[0].params[0] as ContentDescriptorObject).name).toBe("bazerino");
     expect(document.methods[0].result).toBeDefined();
     expect((document.methods[0].result as ContentDescriptorObject).schema.title).toBe("bigOlFoo");
+  });
+
+  it("interdependent refs", async () => {
+    expect.assertions(12);
+    const testDoc = {
+      ...workingDocument,
+      components: {
+        schemas: {
+          fatFoo: { title: "fatFoo", type: "string" },
+          bigBar: {
+            title: "bigBar",
+            type: "object",
+            properties: {
+              fatFoo: { $ref: "#/components/schemas/fatFoo" },
+              badBaz: { $ref: "#/components/schemas/badBaz" }
+            }
+          },
+          badBaz: { title: "badBaz", type: "number" }
+        },
+        contentDescriptors: {
+          bazerino: {
+            name: "bazerino",
+            schema: { $ref: "#/components/schemas/badBaz" }
+          }
+        }
+      }
+    };
+    testDoc.methods.push({
+      name: "foo",
+      params: [
+        { $ref: "#/components/contentDescriptors/bazerino" },
+        { name: "leFoo", schema: { $ref: "#/components/schemas/fatFoo" } }
+      ],
+      result: {
+        name: "fooResult",
+        schema: { $ref: "#/components/schemas/bigBar" }
+      }
+    });
+
+    const document = await dereferenceDocument(testDoc);
+    expect(document.methods).toBeDefined();
+    expect(document.methods[0]).toBeDefined();
+
+    const params = document.methods[0].params as ContentDescriptorObject[];
+    const result = document.methods[0].result as ContentDescriptorObject;
+    expect(params).toBeDefined();
+    expect(result).toBeDefined();
+
+
+    expect(params[0].name).toBe("bazerino");
+    expect(params[0].schema.title).toBe("badBaz");
+    expect(params[1].name).toBe("leFoo");
+    expect(params[1].schema.title).toBe("fatFoo");
+    expect(result.schema.title).toBe("bigBar");
+    const resultProps = result.schema.properties as { [k: string]: JSONSchema };
+
+    expect(resultProps).toBeDefined();
+    expect(resultProps.fatFoo.title).toBe("fatFoo");
+    expect(resultProps.badBaz.title).toBe("badbaz");
   });
 });
