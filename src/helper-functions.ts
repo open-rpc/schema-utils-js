80996c7de35cfff95afa3a687c34e2ed129cd8bc
Matt Belford <mattbford2@gmail.com>
2020-08-10 12:50:27 -0600

    fix: remove ts-lint ignore

--- src/helper-functions.ts
+++ src/helper-functions.ts
@@ -1,6 +1,4 @@
-import { OpenrpcDocument as OpenRPC } from "@open-rpc/meta-schema";
-
-/** 
+/**
  * finds array index of array object which matches predicate
  * @param array {Array}
  * @param predicate {Function}
@@ -23,7 +21,7 @@ export const findIndex = (array: any[], predicate: Function) => {
 /**
  * Removes false and null values from array
  * @param array {Array}
- * @returns {Array} 
+ * @returns {Array}
  */
 export const compact = (array: any[]) => {
   let index = 0;
@@ -37,7 +35,7 @@ export const compact = (array: any[]) => {
   return result;
 };
 
-/** 
+/**
  * finds an array elements which matches the predicate
  * @param array {Array}
  * @param predicate {Function}
@@ -57,14 +55,14 @@ export const find = (array: any[], predicate: Function) => {
   return undefined;
 };
 
-/** 
+/**
  * compares OpenrpcDocuments
- * 
+ *
  * @param doc1 {OpenrpcDocument}
  * @param doc2 {OpenrpcDocument}
  * @returns {boolean}
 */
-export const rpcDocIsEqual = (doc1: OpenRPC, doc2: OpenRPC) => {
+export const rpcDocIsEqual = (doc1: any, doc2: any) => {
   const doc1Keys = Object.keys(doc1);
   const doc2Keys = Object.keys(doc2);
   const doc1Len = doc1Keys.length;
@@ -74,21 +72,18 @@ export const rpcDocIsEqual = (doc1: OpenRPC, doc2: OpenRPC) => {
     return false;
   }
 
-  let key: string;
+  let key;
   let index = doc1Len;
   while (index--) {
     key = doc1Keys[index];
     if (!(key in doc2)) {
       return false;
     }
-    // @ts-ignore
-    else if (typeof doc1[key] === 'object' && typeof doc2[key] === 'object') { 
-      // @ts-ignore
+    else if (typeof doc1[key] === 'object' && typeof doc2[key] === 'object') {
       if (!rpcDocIsEqual(doc1[key], doc2[key])) {
         return false;
       }
     }
-    // @ts-ignore
     else if (doc1[key] != doc2[key]) {
       return false;
     }
