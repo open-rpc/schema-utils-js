cfc719bcb92d5c8849c3f0f765ca671e97c9d4ec
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2026-02-17 15:01:09 -0800

    fix(deps): upgrade ajv from v6 to v8 to resolve ReDoS vulnerability
    BREAKING CHANGE: `ErrorObject.dataPath` is renamed to `ErrorObject.instancePath` in ajv v8

--- src/method-call-validator/parameter-validation-error.test.ts
+++ src/method-call-validator/parameter-validation-error.test.ts
@@ -3,10 +3,11 @@ import { ErrorObject } from "ajv";
 
 describe("ParameterValidationError", () => {
   const errorObj = {
-    dataPath: "abc",
+    instancePath: "abc",
     keyword: "123",
     params: {},
     schemaPath: "1/2/3",
+    message: "test error",
   } as ErrorObject;
 
   it("can be instantiated", () => {
