c24200bd6bf699da2ea13599b133f06891cfae5b
Zane Starr <zcstarr@gmail.com>
2026-02-16 16:31:33 -0800

    fix: early exit on undefined document

--- src/validate-open-rpc-document.ts
+++ src/validate-open-rpc-document.ts
@@ -51,6 +51,8 @@ export class OpenRPCDocumentValidationError implements Error {
 export default function validateOpenRPCDocument(
   document: OpenRPC
 ): OpenRPCDocumentValidationError | true {
+  if (!document) throw new Error("schema-utils-js: Internal Error - document is undefined");
+
   const ajv = new Ajv();
   ajv.addSchema(JsonSchemaMetaSchema, "https://meta.json-schema.tools");
   let extMetaSchema = getExtendedMetaSchema(document.openrpc);
